39cbed713283cb93434a3e951622ae4d0d6b77a1
MariliaRonssen <marilia.er@gmail.com>
2022-11-01 09:52:11 -0300

    uc10

---
+++ UC10-T13/mania-t13/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ UC10-T13/mania-t13/src/app/views/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-Login',
+  templateUrl: './Login.component.html',
+  styleUrls: ['./Login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private loginService: LoginService) { }
+
+  ngOnInit(): void {
+  }
+
+  userModel = new User();
+  receberDados() {
+    this.loginService.login(this.userModel).subscribe((response) => {
+      console.log(response.body.user.name)
+    })
+  }
+}
