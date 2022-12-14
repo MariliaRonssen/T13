34d6c226dea0e8716a5c795d0b37aa1009823730
MariliaRonssen <marilia.er@gmail.com>
2022-12-14 12:24:58 -0300

    agora com os prints das mensagem

--- UC10-T13/UC10-encontro remoto 2/src/app/views/login/login.component.ts
+++ UC10-T13/UC10-encontro remoto 2/src/app/views/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
 import { Router } from '@angular/router';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-Login',
@@ -22,20 +23,33 @@ export class LoginComponent implements OnInit {
 
   receberDados() {
     this.loginService.login(this.userModel).subscribe((response) => {
-      // alert("deu certo");
+      alert("deu certo");
       this.mensagem = 'Usuário logado com sucesso';
       this.router.navigateByUrl("/")
     }, (respostaErro) => {
       // alert("deu errado");
-       alert(this.mensagem = 'Usuário ou senha incorreto')
+      // (this.mensagem = 'Usuário ou senha incorreto')
+      
+      if (respostaErro.error == "Email and password are required") {
+        this.mensagem = "Digite email e senha"
+      } else if (
+        this.mensagem = respostaErro.error)
+        if (respostaErro.error == "Cannot find user") {
+          this.mensagem = "usuario não encontrado"
+        } else if (
+          this.mensagem = respostaErro.error)
+          if (respostaErro.error == "Password is too short") {
+            this.mensagem = "senha muito curta"
+          } else if (
+            this.mensagem = respostaErro.error)
+            if (respostaErro.error == "Incorrect password") {
+              this.mensagem = "senha errada"
+            } else {
+              this.mensagem = respostaErro.error;
+            }
 
-      if(respostaErro.error == "Cannot find user"){
-        this.me ="usuario não encontrado"
-      }else
-      this.me = respostaErro.error;
-      
-      
     }
+
     )
   }
 }
